bc6074400c3baf2296383c120038cf12dfe6f00a
wintermute-a\ryo <kiwifeather@gmail.com>
2025-09-02 15:21:53 +0900

    9/2分

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody; //Playerに付いているRigidbodyを扱う変数
 
-    float axisH: //水平方向の入力を格納する変数
+    float axisH; //水平方向の入力を格納する変数
+    float speed = 3.0f; //速度設定
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,14 +16,18 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {  {
+    { 
             //Velocityの元となる値の取得(右1.0f、左-1.0f、何もなければ0)
 
             axisH = Input.GetAxisRaw("Horizontal");
 
+        }
+
+            //一秒間に50fps繰り返すように制御しながら繰り返すメソッド
+            private void FixedUpdate()
+    {
             //Verocityに代入
-            rbody.linearVelocity = new Vector2(axisH,0);
+            rbody.linearVelocity = new Vector2(axisH*speed,rbody.linearVelocity.y);
         }
 
     }
-}
