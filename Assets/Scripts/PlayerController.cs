c777c5876f70071083b3c0d712efd5c8f0450bf7
wintermute-a\ryo <kiwifeather@gmail.com>
2025-09-03 15:27:26 +0900

    アニメーション作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,18 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rbody; //Playerに付いているRigidbodyを扱う変数
+    [Header("プレイヤーの能力値")]
+    public float speed = 3.0f; //プレイヤーのスピードを調整
+    public float jumpPower = 9.0f; //ジャンプ力
 
-    float axisH; //水平方向の入力を格納する変数
-    float speed = 3.0f; //速度設定
+    [Header("地面判定の対象レイヤー")]
+    public LayerMask groundLayer; //地面レイヤーを指名するための変数
 
-    public float jumpPower = 9.0f; //ジャンプ力設定
-    bool gojump = false; //ジャンプするかどうかのフラグ
-    bool onGround = false; //地面に接地しているかどうかのフラグ
-
-    public LayerMask groundLayer; //地面のレイヤーを設定するための変数
-    
-
+    Rigidbody2D rbody; //PlayerについているRigidbody2Dを扱うための変数
+    float axisH; //入力の方向を記憶するための変数
+    bool goJump = false; //ジャンプフラグ（true:真on、false:偽off)
+    bool onGround = false; //地面にいるかどうかの判定（地面にいる：true、地面にいない：false）
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,53 +23,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Velocityの元となる値の取得(右1.0f、左-1.0f、何もなければ0)
-
+        //Velocityの元となる値の取得（右なら1.0f、左なら-1.0f、なにもなければ0)
         axisH = Input.GetAxisRaw("Horizontal");
 
-        //右だったら右を向く、左だったら左を向く
         if (axisH > 0)
         {
+            //右を向く
             transform.localScale = new Vector3(1, 1, 1);
         }
         else if (axisH < 0)
         {
+            //左を向く
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        //ジャンプの入力を取得
+        //GetButtonDownメソッド→引数に指定したボタンが押されたらtrueを返す、押されていなければfalseを返す
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            Jump(); //Jumpメソッドの発動
         }
 
     }
 
-    //一秒間に50fps繰り返すように制御しながら繰り返すメソッド
-    private void FixedUpdate()
+    //1秒間に50回(50fps)繰り返すように制御しながら行う繰り返しメソッド
+    void FixedUpdate()
     {
-        //地面判定をサークルキャストで行い、その結果を変数onGroundに格納する
+        //地面判定をサークルキャストで行って、その結果を変数onGroundに代入
         onGround = Physics2D.CircleCast(
-            transform.position, //発射位置
-            0.3f, //サークルの半径
-            new Vector2(0,1.0F), //発射方向
-            groundLayer //対象となるレイヤー
+            transform.position,   //発射位置＝プレイヤーの位置（基準点）
+            0.2f,                   //調査する円の半径
+            new Vector2(0, 1.0f),  //発射方向 ※下方向
+            0,                      //発射距離
+            groundLayer        //対象となるレイヤー情報 ※LayerMask
             );
 
-
-        //Verocityに代入
+        //Velocityに値を代入
         rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
 
-        if (gojump == true)
+        //ジャンプフラグが立ったら
+        if (goJump)
         {
-            //ジャンプさせる
+            //ジャンプさせる→プレイヤーを上に押し出す
             rbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
-            gojump = false;
+            goJump = false; //フラグをOFFに戻す
         }
     }
 
+    //ジャンプボタンがおされた時に呼び出されるメソッド
     void Jump()
     {
-        gojump = true;
+        if (onGround)
+        {
+            goJump = true; //ジャンプフラグをON
+        }
     }
+
 }
